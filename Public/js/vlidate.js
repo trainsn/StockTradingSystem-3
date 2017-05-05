5fbb8d8ddc1b095ce262c8f39b65b186a5eefbd8
alicelh <820267577@qq.com>
2017-05-05 14:13:29 +0800

    some correct on stylesheet

--- Public/js/vlidate.js
+++ Public/js/vlidate.js
@@ -36,7 +36,6 @@
             var nameRegex=/^\w{6,20}$/;
             if(!nameRegex.test(name)){
                 document.getElementById("nameInfo").innerHTML="用户名必须在6个字母或数字以上，不超过20";
-                document.forms["myForm"].username.focus();
                 return false;
             }else{
                 document.getElementById("nameInfo").innerHTML="";
@@ -55,7 +54,6 @@
             var passwordRegex=/^[0-9A-Za-z_]\w{7,15}$/;
             if(!passwordRegex.test(password)){
                 document.getElementById("passwordInfo").innerHTML="密码长度必须在8个字符到16个字符之间";
-                document.forms["myForm"].password.focus();
                 return false;
             }else{
                 document.getElementById("passwordInfo").innerHTML="";
@@ -70,7 +68,6 @@
             //校验密码和上面密码必须一致
             if(repassword!==password){
                 document.getElementById("repasswordInfo").innerHTML="两次输入的密码不一致";
-                document.forms["myForm"].repassword.focus();
                 return false;
             }else if(repassword==password){
                 document.getElementById("repasswordInfo").innerHTML="";
@@ -84,7 +81,6 @@
           var dotpos=x.lastIndexOf(".");
           if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length){
               document.getElementById("emailInfo").innerHTML="不是一个有效的 e-mail 地址";
-              document.forms["myForm"].email.focus();
               return false;
           }
           else{
@@ -97,7 +93,6 @@
           var tel=document.forms["myForm"].phone_number.value;
           if(!/^1[3|4|5|7|8][0-9]{9}$/.test(tel)){
             document.getElementById("phoneInfo").innerHTML="请填写正确的手机号码！";
-            document.forms["myForm"].phone_number.focus();
             return false;
           }
           else{
